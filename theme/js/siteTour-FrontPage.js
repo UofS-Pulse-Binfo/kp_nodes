fa52624544c3c965afa4e4a7964b3bf3949cad03
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2016-02-11 10:47:36 -0600

    Added frontpage tour step for the login box.

--- theme/js/siteTour-FrontPage.js
+++ theme/js/siteTour-FrontPage.js
@@ -47,20 +47,36 @@
         {
           selector: '#block-menu-menu-our-research',
           present: $('#block-menu-menu-our-research').length
+        },
+        {
+          selector: '#block-user-login',
+          present: $('#block-user-login').length
         }
       ];
       
       // Define a function to get the selector of the next step that is present.
       var getNextSelector = function(currentIndex) {
-        if (steps[currentIndex+1].present) {
-          return steps[currentIndex+1].selector;
+        checkIndex = currentIndex+1;
+        while (checkIndex in steps) {
+          if (steps[checkIndex].present) {
+            return steps[checkIndex].selector;
+          }
+          checkIndex++;
         }
-        if (steps[currentIndex+2].present) {
-          return steps[currentIndex+2].selector;
+        return '#frontpage-tour-button';
+      }
+      
+      // Define a function to determine if this is the last step.
+      var isLast = function(currentIndex) {
+        console.log('checking '+currentIndex);
+        checkIndex = currentIndex+1;
+        while (checkIndex in steps) {
+          if (steps[checkIndex].present) {
+            return false;
+          }
+          checkIndex++;
         }
-        if (steps[currentIndex+3].present) {
-          return steps[currentIndex+3].selector;
-        }
+        return true;
       }
 
       // STEP #1: Introduce navigation menu if the user is logged in.
@@ -71,7 +87,7 @@
           title: 'Add Content',
           text : 'You can add content including registering sequence data or describing markers, germplasm or projects, here.',
           attachTo : steps[0].selector + ' left',
-          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(0)),
+          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(0), isLast(0)),
         });
         last = steps[0].selector;
       }
@@ -83,7 +99,7 @@
           title: 'Bioinformatic Tools',
           text : 'If you have a sequence and need to find its homologous entry in KnowPulse, you can use the BLAST. Alternatively, if you would like to graphically explore the crop genomes, you can use the corresponding JBrowse.',
           attachTo : steps[1].selector + ' left',
-          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(1)),
+          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(1), isLast(1)),
         });
         last = steps[1].selector;
       }
@@ -95,7 +111,7 @@
           title: 'Genomic Data',
           text : 'If you are looking for a particular sequence, variant or marker by name you can use the corresponding searches in this menu. To search for markers that are polymorphic in specific germplasm, use the genotype searches.',
           attachTo : steps[2].selector + ' left',
-          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(2)),
+          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(2), isLast(2)),
         });
         last = steps[2].selector;
       }
@@ -107,7 +123,7 @@
           title: 'Germplasm',
           text : 'If you would like more information about a particular variety, accession or cross then select the crop to access a general search. More specific searches will be exposed in this menu once you select the crop.',
           attachTo : steps[3].selector + ' left',
-          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(3)),
+          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(3), isLast(3)),
         });
         last = steps[3].selector;
       }
@@ -119,10 +135,22 @@
           title: 'Our Research',
           text : 'If you would like more information about the University of Saskatchewan Pulse group, you can access our projects, publications and general crop/species information in this menu.',
           attachTo : steps[4].selector + ' left',
-          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, '#frontpage-tour-button', true),
+          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(4), isLast(4)),
+        });
+        last = steps[4].selector;
+      }
+
+      // STEP #6: Login
+      //-----------------------------------------
+      if (steps[5].present) {
+        tour.addStep('fontpage-5', {
+          title: 'User Login',
+          text : 'If you would like to access the Lentil genome, add content or retrieve information about our germplasm, you will need to login.</p><p>Additionally your account will need authorization. For the Lentil Genome this can be obtained by <a href="http://knowpulse.usask.ca/portal/lentil-genome">clicking here</a>; whereas, to add content or browse our germplasm you will need to talk to Lacey Sanderson or Kirstin Bett directly.',
+          attachTo : steps[5].selector + ' left',
+          buttons: Drupal.kptheme.shepherd.scrollingButtons(last, getNextSelector(5), isLast(5)),
         });
       }
-            
+         
       // Now actually start the tour
       tour.start();
     }
